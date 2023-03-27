dd50e603124a68a2e5c079c7e31b2d82a76a8e91
erjondemaku <ed55981@ubt-uni.net>
2023-03-28 01:05:15 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
+            Erjon Demaku
+
           </p>
           <div>
             <a
