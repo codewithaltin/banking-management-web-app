c37ad2cbc39726efdf2f8f81ffffd2754cc0ac0c
erjonberishaa <eb55980@ubt-uni.net>
2023-04-16 02:18:29 +0200

    Charity Section- content added

--- pages/index.js
+++ pages/index.js
@@ -295,7 +295,7 @@ export default function Index() {
 
             <div className="w-full md:w-4/12 px-12 md:px-4 ml-auto mr-auto mt-48">
               <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                <i className="fas fa-drafting-compass text-xl"></i>
+                <i className="far fa-credit-card text-xl"></i>
               </div>
               <h3 className="text-3xl mb-2 font-semibold leading-normal text-blueGray-900">
                 Financial Empowerment
@@ -349,28 +349,25 @@ export default function Index() {
             <div className="w-full md:w-5/12 ml-auto px-12 md:px-4">
               <div className="md:pr-12">
                 <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                  <i className="fas fa-file-alt text-xl"></i>
+                  <i className="fas fa-hand-holding-heart text-xl"></i>
                 </div>
-                <h3 className="text-3xl font-semibold">
-                  Complex Documentation
+                <h3 className="text-3xl font-semibold text-blueGray-900">
+                Elevate Your Giving
                 </h3>
-                <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                  This extension comes a lot of fully coded examples that help
-                  you get started faster. You can adjust the colors and also the
-                  programming language. You can change the text and images and
-                  you're good to go.
+                <p className="mt-4 text-lg leading-relaxed text-blueGray-700">
+                At “FuturBanking”, we believe in making a positive impact on the world. By using our online banking platform, you can easily donate to a variety of charitable organizations that we've partnered with. Join us in giving back to your community and make a difference today!
                 </p>
                 <ul className="list-none mt-6">
                   <li className="py-2">
                     <div className="flex items-center">
                       <div>
                         <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                          <i className="fas fa-fingerprint"></i>
+                          <i className="fas fa-donate"></i>
                         </span>
                       </div>
                       <div>
-                        <h4 className="text-blueGray-500">
-                          Built by Developers for Developers
+                        <h4 className="text-blueGray-600">
+                          Be a Part of Positive Change
                         </h4>
                       </div>
                     </div>
@@ -379,12 +376,12 @@ export default function Index() {
                     <div className="flex items-center">
                       <div>
                         <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                          <i className="fab fa-html5"></i>
+                          <i className="fas fa-dove"></i>
                         </span>
                       </div>
                       <div>
-                        <h4 className="text-blueGray-500">
-                          Carefully crafted code for Components
+                        <h4 className="text-blueGray-600">
+                          Spread Your Wings of Generosity
                         </h4>
                       </div>
                     </div>
@@ -393,12 +390,12 @@ export default function Index() {
                     <div className="flex items-center">
                       <div>
                         <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                          <i className="far fa-paper-plane"></i>
+                          <i className="fas fa-heart"></i>
                         </span>
                       </div>
                       <div>
-                        <h4 className="text-blueGray-500">
-                          Dynamic Javascript Components
+                        <h4 className="text-blueGray-600">
+                          From Your Heart to Others
                         </h4>
                       </div>
                     </div>
@@ -415,7 +412,7 @@ export default function Index() {
                   transform:
                     "scale(1) perspective(1040px) rotateY(-11deg) rotateX(2deg) rotate(2deg)",
                 }}
-                src="/img/documentation.png"
+                src="/img/charity.jpg"
               />
             </div>
           </div>
