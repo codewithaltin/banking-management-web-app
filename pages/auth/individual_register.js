6155a6e6b590f102eb504fe3b762d2f09a25cbd5
Altin Morina <altinmorina2002@gmail.com>
2023-04-16 17:33:10 +0200

    React Hook Installed

--- pages/auth/individual_register.js
+++ pages/auth/individual_register.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 // layout for page
 
 import Auth from "layouts/Auth.js";
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <div className="container mx-auto px-4 h-full">
@@ -39,7 +48,7 @@ export default function Register() {
                 <div className="text-blueGray-400 text-center mb-3 font-bold">
                   <small>Or sign up with credentials</small>
                 </div>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="relative w-full mb-3">
                     <label
                       className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
@@ -48,6 +57,7 @@ export default function Register() {
                       Name
                     </label>
                     <input
+                      {...register("name", { required: true })}
                       type="email"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder=" Full Name"
@@ -131,7 +141,7 @@ export default function Register() {
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                       type="button"
                     >
-                      Next
+                      Submit
                     </button>
                   </div>
                 </form>
