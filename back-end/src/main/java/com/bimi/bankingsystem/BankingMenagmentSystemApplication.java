7922ff95635a3a3a4ebad2be73954cd0e0f02248
erjonberishaa <eb55980@ubt-uni.net>
2023-04-20 01:10:17 +0200

    Back End added

---
+++ back-end/src/main/java/com/bimi/bankingsystem/BankingMenagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bimi.bankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingMenagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingMenagmentSystemApplication.class, args);
+	}
+
+}
