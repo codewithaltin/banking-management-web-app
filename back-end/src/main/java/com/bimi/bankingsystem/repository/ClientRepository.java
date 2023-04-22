9962b72aa24140995c662aab25acbe433eb1ac3c
Altin Morina <altinmorina2002@gmail.com>
2023-04-22 18:32:25 +0200

    First Crud Imeplemented

---
+++ back-end/src/main/java/com/bimi/bankingsystem/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.bimi.bankingsystem.repository;
+
+import com.bimi.bankingsystem.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client,Integer> {
+}
