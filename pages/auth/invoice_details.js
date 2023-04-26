17ce4e9ff6bf9180cce8c4e2020754f6a87e2ae6
erjondemaku <ed55981@ubt-uni.net>
2023-04-26 23:55:39 +0200

    date added

--- pages/auth/invoice_details.js
+++ pages/auth/invoice_details.js
@@ -84,6 +84,18 @@ export default function Invoice() {
                                                     required
                                                     placeholder="Client's email"
                                                 />
+                                                <label
+                                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                                >
+                                                    Due Date
+                                                </label>
+                                                <input
+                                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                                                    id="dueDate"
+                                                    name="dueDate"
+                                                    type="date"
+                                                    required
+                                                />
                                             </div>
 
                                             <div
