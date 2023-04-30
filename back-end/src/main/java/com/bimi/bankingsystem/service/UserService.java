54bd82e10fcb859585c4425b872c85114c1ccd06
BrahimSylejmani <bs57810@ubt-uni.net>
2023-04-30 23:44:12 +0200

    UserService added

---
+++ back-end/src/main/java/com/bimi/bankingsystem/service/UserService.java
@@ -0,0 +1,18 @@
+package com.bimi.bankingsystem.service;
+
+import com.bimi.bankingsystem.model.User;
+import java.util.*;
+
+public interface UserService {
+
+    User saveUser(User user);
+
+    List<User> getAllUsers();
+
+    User getUserById(Long id);
+
+    boolean deleteUser(Long id);
+
+    User updateUser(Long id, User user);
+
+}
