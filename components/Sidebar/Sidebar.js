4cfc194ecf2ea83a726325864a6f8417af74d8d0
BrahimSylejmani <bs57810@ubt-uni.net>
2023-05-03 22:52:12 +0200

    Side bar change

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -129,6 +129,27 @@ export default function Sidebar() {
               </li>
               <li className="items-center">
                 <Link
+                  href="/admin/userlist"
+                  className={
+                    "text-xs uppercase py-3 font-bold block " +
+                    (router.pathname.indexOf("/admin/contactlist") !== -1
+                      ? "text-lightBlue-500 hover:text-lightBlue-600"
+                      : "text-blueGray-700 hover:text-blueGray-500")
+                  }
+                >
+                  <i
+                    className={
+                      "fas fa-table mr-2 text-sm " +
+                      (router.pathname.indexOf("/admin/userlist") !== -1
+                        ? "opacity-75"
+                        : "text-blueGray-300")
+                    }
+                  ></i>{" "}
+                  Contact Forms List
+                </Link>
+              </li>
+              <li className="items-center">
+                <Link
                   href="/admin/settings"
                   className={
                     "text-xs uppercase py-3 font-bold block " +
