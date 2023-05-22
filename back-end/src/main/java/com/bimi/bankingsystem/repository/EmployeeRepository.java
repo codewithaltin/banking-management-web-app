adcc650f03fa0f81a2f3b270b80c743b8df8b312
Altin Morina <altinmorina2002@gmail.com>
2023-05-22 15:32:27 +0200

    Employee Repository Completed on Server Side

---
+++ back-end/src/main/java/com/bimi/bankingsystem/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.bimi.bankingsystem.repository;
+
+import com.bimi.bankingsystem.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
