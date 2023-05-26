68db4dc95a7e1ea7b1b71f249ef2c5ae780eb3b0
erjondemaku <ed55981@ubt-uni.net>
2023-05-26 18:47:49 +0200

    beneficiary page created

---
+++ pages/beneficiary.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Navbar from "components/Navbars/IndexNavbar.js";
+import Footer from "components/Footers/Footer.js";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
