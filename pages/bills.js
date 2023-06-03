7281c3a166dee20ef28807ba5c58a430d5b1591d
Altin Morina <altinmorina2002@gmail.com>
2023-06-04 00:34:30 +0200

    Request Money Form Created

---
+++ pages/bills.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Auth from "layouts/Auth.js";
+
+const bills = () => {
+  return (
+    <div className="container mx-auto px-4 h-full">
+      <div className="flex content-center items-center justify-center h-full">
+        <div className="w-full lg:w-6/12 px-4">
+          <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+            <div className="rounded-t mb-0 px-6 py-6">
+              <div className="text-center mb-3">
+                <h6 className="text-blueGray-500 text-sm font-bold">
+                  Request Money
+                </h6>
+              </div>
+
+              <hr className="mt-6 border-b-1 border-blueGray-300" />
+            </div>
+            <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+              <form>
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder="p.s example@gmail.com"
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <small role="alert" className="text-red-500 "></small>
+                </div>
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    Amount
+                  </label>
+                  <input
+                    type="number"
+                    name="amount"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    placeholder="p.s example@gmail.com"
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <small role="alert" className="text-red-500 "></small>
+                  <div className="text-center mt-6">
+                    <input
+                      className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      type="submit"
+                      value="Request Money"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+bills.layout = Auth;
+
+export default bills;
