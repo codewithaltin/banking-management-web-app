fcad19e74c71c226973143e0aea9815d38d3c11e
Altin Morina <altinmorina2002@gmail.com>
2023-06-05 16:52:48 +0200

    Application Configuration Added.

--- back-end/src/main/java/com/bimi/bankingsystem/config/JwtAuthenticationFilter.java
+++ back-end/src/main/java/com/bimi/bankingsystem/config/JwtAuthenticationFilter.java
@@ -6,6 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +19,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -31,5 +36,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail  = jwtService.extractUserName(jwt);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails =this.userDetailsService.loadUserByUsername(userEmail);
+
+        }
     }
 }
--- back-end/src/main/java/com/bimi/bankingsystem/config/JwtService.java
+++ back-end/src/main/java/com/bimi/bankingsystem/config/JwtService.java
@@ -1,27 +1,61 @@
 package com.bimi.bankingsystem.config;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY="2646294A404E635266546A576E5A7234753778214125442A472D4B6150645367";
     public String extractUserName(String token) {
-        return null;
+        return extractClaim(token,Claims::getSubject);
     }
-
-    private Claims extractAllClains(String token){
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token){
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+    public String generateToken(UserDetails userDetails){
+        return  generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken( Map<String, Object> extraClaims, UserDetails userdetails){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userdetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Key getSigningKey() {
