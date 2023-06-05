61c221d2a88a398538dd953e7c48c7d7e59d42ae
Altin Morina <altinmorina2002@gmail.com>
2023-06-05 15:21:50 +0200

    Json Web Tokens Dependencies added.

--- back-end/src/main/java/com/bimi/bankingsystem/config/JwtAuthenticationFilter.java
+++ back-end/src/main/java/com/bimi/bankingsystem/config/JwtAuthenticationFilter.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -23,11 +23,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
 
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
            filterChain.doFilter(request,response);
            return;
         }
         jwt = authHeader.substring(7);
+        userEmail  = jwtService.extractUserName(jwt);
     }
 }
---
+++ back-end/src/main/java/com/bimi/bankingsystem/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.bimi.bankingsystem.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUserName(String token) {
+        return null;
+    }
+}
