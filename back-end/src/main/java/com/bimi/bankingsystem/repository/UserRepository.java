502dc6e405e10a5bd62c1e429b44e3af42219597
Altin Morina <altinmorina2002@gmail.com>
2023-06-05 15:15:02 +0200

    JwtAuthenticationFilter and base functions added.

--- back-end/src/main/java/com/bimi/bankingsystem/repository/UserRepository.java
+++ back-end/src/main/java/com/bimi/bankingsystem/repository/UserRepository.java
@@ -5,7 +5,11 @@ import com.bimi.bankingsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
 
