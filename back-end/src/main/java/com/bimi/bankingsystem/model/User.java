59052787a1b787abb6b305537574f293f15b9a40
Altin Morina <altinmorina2002@gmail.com>
2023-09-11 13:42:21 +0200

    Bug fixes

--- back-end/src/main/java/com/bimi/bankingsystem/model/User.java
+++ back-end/src/main/java/com/bimi/bankingsystem/model/User.java
@@ -13,7 +13,6 @@ import java.util.Set;
 @Setter
 @AllArgsConstructor
 @Table ( name="user")
-
 public class User {
     @Setter(AccessLevel.PROTECTED) @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,29 +33,15 @@ public class User {
     @NonNull
     private String role;
 
-<<<<<<< HEAD
-    public User(long id, String firstName, String lastName, String email, String phoneNumber, String password,String role) {
-=======
-    public Set<SavingGoal> getSavingGoals() {
-        return savingGoals;
-    }
-
     @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private Set<SavingGoal> savingGoals;
 
 
-
-    public User(long id, String firstName, String lastName, String emailId, String phoneNumber, String password,String role) {
->>>>>>> f3177e905e0e03a3b4f0e65f81587dd7eb450432
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailId = emailId;
-        this.phoneNumber = phoneNumber;
-        this.password = password;
-        this.role = role;
+    public Set<SavingGoal> getSavingGoals() {
+        return savingGoals;
     }
+
     public User(){
         role = "USER";
     }
--- pages/_app.js
+++ pages/_app.js
@@ -57,7 +57,6 @@ export default class MyApp extends App {
         </SessionProvider>
         */}
         {/* <StepContext> */}
-<<<<<<< HEAD
         <React.Fragment>
           <Head>
             <meta
@@ -71,22 +70,6 @@ export default class MyApp extends App {
             <Component {...pageProps} />
           </Layout>
         </React.Fragment>
-=======
-          <React.Fragment>
-            <Head>
-              <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no"
-              />
-
-              <title>Futur Banking</title>
-            </Head>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </React.Fragment>
-        {/* </StepContext> */} 
->>>>>>> f3177e905e0e03a3b4f0e65f81587dd7eb450432
       </>
     );
   }
