ffe556ac49c9867fc6afdcd53a029e9ca5e15f39
BrahimSylejmani <bs57810@ubt-uni.net>
2023-09-22 18:36:11 +0200

    Auth Layout Bug fixed

--- layouts/Auth.js
+++ layouts/Auth.js
@@ -10,18 +10,24 @@ import AdminNavbar from "components/Navbars/AdminNavbar.js";
 export default function Auth({ children }) {
   return (
     <>
+      <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
         <AdminNavbar />
         {/* Header */}
         <div className="relative bg-blueGray-800 md:pt-32 pb-32 pt-12">
-          <div className="px-4 md:px-10 mx-auto w-full">
-            <div>
-              {/* Card stats */}
-              <div className="flex flex-wrap"></div>
+        <div className="px-4 md:px-10 mx-auto w-full">
+          <div>
+            {/* Card stats */}
+            <div className="flex flex-wrap">
+
             </div>
           </div>
         </div>
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">{children}</div>
+      </div>
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          {children}
+
+        </div>
       </div>
     </>
   );
