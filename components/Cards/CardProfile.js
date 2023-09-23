bf56500b5b352ff87ed7997928a557a6af9d46b1
Altin Morina <altinmorina2002@gmail.com>
2023-09-23 02:19:24 +0200

    Card Settings layout completed

--- components/Cards/CardProfile.js
+++ components/Cards/CardProfile.js
@@ -5,40 +5,35 @@ export default function CardProfile(profile) {
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
-        <div className="w-full px-4 flex justify-center">
-          <div>
-            <img
-              alt="..."
-              src="/img/blank-profile-picture.webp"
-              className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-            />
-          </div>
-        </div>
-        <div className="px-6">
-          <button className="mb-2 bg-blue-500 hover:bg-blue-700 text-blueGray-500 font-bold py-2 px-4 rounded mt-4 top-0 "></button>
-        </div>
-        <div className="text-center mt-12 ">
-          <h3 className="text-2xl font-bold leading-normal text-blueGray-700 mb-2">
-            {profile.profile.firstName + " " + profile.profile.lastName}
-            <div className="mb-2 text-blueGray-600 m-8  text-lg">
-              <i className="fas fa-euro-sign mr-2  text-blueGray-400"></i>
-              {profile.profile.balance + " EUR"}
+        <div className="w-full px-4 flex justify-center"></div>
+        <div className="m-8">
+          <div className="flex justify-between">
+            <div>
+              <h3 className="text-3xl  font-bold leading-normal text-blueGray-700 mb-2 ">
+                {profile.profile.firstName + " " + profile.profile.lastName}{" "}
+              </h3>
             </div>
-          </h3>
-          <div className="flex m-8 justify-between">
-            <div className="mb-2 text-blueGray-600 m-8  text-lg ">
+
+            <div>
+              <h3 className="text-xl  font-bold leading-normal text-blueGray-700 mb-2 ">
+                {profile.profile.balance + " EUR"}{" "}
+              </h3>
+            </div>
+          </div>
+          <div className="flex  justify-between">
+            <div className="mb-2 text-blueGray-600 mt-8  text-lg ">
               <i className="fas fa-credit-card mr-2  leading-normal  text-blueGray-400"></i>
               {profile.profile.accountNumber}
             </div>
-            <div className="mb-2 text-blueGray-600 m-8  text-lg">
-              <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
+            <div className="mb-2 text-blueGray-600  text-lg">
+              <i className="fas fa-map-marker-alt mr-2 text-lg mt-8 text-blueGray-400"></i>
               {profile.profile.city}
             </div>
-            <div className="mb-2 text-blueGray-600 m-8  text-lg">
+            <div className="mb-2 text-blueGray-600 mt-8  text-lg">
               <i className="fas fa-envelope mr-2 text-lg text-blueGray-400"></i>
               {profile.profile.email}
             </div>
-            <div className="mb-2 text-blueGray-600  m-8 text-lg">
+            <div className="mb-2 text-blueGray-600  mt-8 text-lg">
               <i className="fas fa-phone mr-2 text-lg text-blueGray-400"></i>
               {profile.profile.phoneNumber}
             </div>{" "}
--- components/Cards/CardSettings.js
+++ components/Cards/CardSettings.js
@@ -32,7 +32,7 @@ const schema = yup
       .matches(phoneReg, "Phone Number is not valid."),
   })
   .required();
-export default function CardSettings(userId) {
+export default function CardSettings(profile) {
   const USER_API_BASE_URL = "http://localhost:8080/api/v1/auth/user/";
   const {
     register,
@@ -58,27 +58,6 @@ export default function CardSettings(userId) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(USER_API_BASE_URL + 3, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const _user = await response.json();
-        setUser(_user);
-        setIsOpen(true);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    if (userId) {
-      fetchData();
-    }
-  }, [userId]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
         const response = await fetch(
           "http://localhost:8080/api/v1/auth/cities",
           {
@@ -98,19 +77,6 @@ export default function CardSettings(userId) {
     fetchData();
   }, []);
 
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  const reset = (e) => {
-    e.preventDefault();
-    setIsOpen(false);
-  };
-
   const handleChange = (event) => {
     const value = event.target.value;
     setUser({ ...user, [event.target.name]: value });
@@ -128,7 +94,7 @@ export default function CardSettings(userId) {
       city: user.city,
     };
     console.log(user.city);
-    const response = await fetch(USER_API_BASE_URL + userId, {
+    const response = await fetch(USER_API_BASE_URL + profile.profile.id, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -139,8 +105,7 @@ export default function CardSettings(userId) {
       throw new Error("Something went wrong");
     }
     const _user = await response.json();
-    setResponseUser(_user);
-    reset(e);
+    setUser(_user);
     Swal.fire("Updated!", "Updated Succesfully!", "success");
   };
   useEffect(() => {
@@ -170,11 +135,12 @@ export default function CardSettings(userId) {
         <div className="rounded-t bg-white mb-0 px-6 py-6">
           <div className="text-center flex justify-between">
             <h6 className="text-blueGray-700 text-xl font-bold">
-              Your information
+              MOS KLIKO UPDATE HALA
             </h6>
             <button
               className="bg-emerald-400 active:bg-blueGray-400 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
               type="button"
+              onClick={updateUser}
             >
               Update Information
             </button>
@@ -183,39 +149,9 @@ export default function CardSettings(userId) {
         <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
           <form>
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              User Information
+              full name{" "}
             </h6>
             <div className="flex flex-wrap">
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Username
-                  </label>
-                  <input
-                    type="text"
-                    className=" px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm outline-none  focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="lucky.jesse"
-                  />
-                </div>
-              </div>
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="jesse@example.com"
-                  />
-                </div>
-              </div>
               <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
                   <label
@@ -227,7 +163,8 @@ export default function CardSettings(userId) {
                   <input
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Lucky"
+                    value={profile.profile.firstName}
+                    onChange={(e) => handleChange(e)}
                   />
                 </div>
               </div>
@@ -242,7 +179,8 @@ export default function CardSettings(userId) {
                   <input
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Jesse"
+                    value={profile.profile.lastName}
+                    onChange={(e) => handleChange(e)}
                   />
                 </div>
               </div>
@@ -251,7 +189,69 @@ export default function CardSettings(userId) {
             <hr className="mt-6 border-b-1 border-blueGray-300" />
 
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              Contact Information
+              contact & address
+            </h6>
+            <div className="flex flex-wrap">
+              <div className="w-full lg:w-12/12 px-4"></div>
+              <div className="w-full lg:w-4/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    email
+                  </label>
+                  <input
+                    type="email"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    value={profile.profile.email}
+                    onChange={(e) => handleChange(e)}
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-4/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    phone number
+                  </label>
+                  <input
+                    type="text"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    value={profile.profile.phoneNumber}
+                    onChange={(e) => handleChange(e)}
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-4/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label
+                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                    htmlFor="grid-password"
+                  >
+                    city
+                  </label>
+                  <select
+                    className=" px-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow border rounder-md focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    name="city"
+                    value={user.city}
+                    onChange={(e) => handleChange(e)}
+                  >
+                    {cities.map((city, index) => (
+                      <option key={index} value={city}>
+                        {city}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+            </div>
+            <hr className="mt-6 border-b-1 border-blueGray-300" />
+
+            <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+              card information
             </h6>
             <div className="flex flex-wrap">
               <div className="w-full lg:w-12/12 px-4">
@@ -260,63 +260,19 @@ export default function CardSettings(userId) {
                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Address
+                    Account number
                   </label>
                   <input
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                    value={profile.profile.accountNumber}
+                    onChange={(e) => handleChange(e)}
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-4/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    City
-                  </label>
-                  <input
-                    type="email"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="New York"
-                  />
-                </div>
-              </div>
-              <div className="w-full lg:w-4/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Country
-                  </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="United States"
-                  />
-                </div>
-              </div>
-              <div className="w-full lg:w-4/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Postal Code
-                  </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue="Postal Code"
-                  />
-                </div>
-              </div>
+              <div className="w-full lg:w-4/12 px-4"></div>
             </div>
-
-            <hr className="mt-6 border-b-1 border-blueGray-300" />
+            {/* <hr className="mt-6 border-b-1 border-blueGray-300" />
 
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
               About Me
@@ -339,7 +295,7 @@ export default function CardSettings(userId) {
                   ></textarea>
                 </div>
               </div>
-            </div>
+            </div> */}
           </form>
         </div>
       </div>
